5744f2d42755adabfcedf56af9d3d3509b0dd977
xblueoceanx <ginnnokizuna@yahoo.co.jp>
2020-07-29 15:28:34 +0900

    [finish]5-4

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -8,15 +8,20 @@ class PostImagesController < ApplicationController
     # binding.pry
     @post_image.user_id = current_user.id
     @post_image.save
-    redirect_to post_images_path
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page]).reverse_order
   end
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
 
   private
