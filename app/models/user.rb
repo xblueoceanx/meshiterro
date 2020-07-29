90074dd2d624cccb6cc547f32ddd5755fb2f6c92
xblueoceanx <ginnnokizuna@yahoo.co.jp>
2020-07-29 13:23:46 +0900

    [Start]5-4

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :post_images, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
 end
