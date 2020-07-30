6a644f87d035293ea2a00d7916befd9fc1638784
xblueoceanx <ginnnokizuna@yahoo.co.jp>
2020-07-30 14:49:59 +0900

    [start]5-6

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   	resource :favorites, only: [:create, :destroy]
   	resources :post_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
